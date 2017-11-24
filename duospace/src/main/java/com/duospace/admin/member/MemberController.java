05a56fb2281b0531fb3da6817f8db31276a4dd49
user1 <user1@user1-PC>
2017-11-24 16:20:37 +0900

    종기 memberController

--- duospace/src/main/java/com/duospace/admin/member/MemberController.java
+++ duospace/src/main/java/com/duospace/admin/member/MemberController.java
@@ -1,4 +1,14 @@
 package com.duospace.admin.member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("admin.member.memberController")
 public class MemberController {
+	@RequestMapping(value="/admin/member/member")
+	public String list(Model model) throws Exception {
+		return ".admin4.menu1.member.member";
+	}
+	
 }
