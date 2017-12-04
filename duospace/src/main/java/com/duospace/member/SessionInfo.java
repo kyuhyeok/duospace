566a3775187e7ec1ff6c38c58c6387e4356cbdc3
kyuhyeok <kyu4745@naver.com>
2017-12-04 13:25:46 +0900

    sessionInfo 추가

--- duospace/src/main/java/com/duospace/member/SessionInfo.java
+++ duospace/src/main/java/com/duospace/member/SessionInfo.java
@@ -1,4 +1,28 @@
 package com.duospace.member;
 
 public class SessionInfo {
+	private int userNum;
+	private String userId;
+	private String userName;
+	
+	public int getUserNum() {
+		return userNum;
+	}
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
 }
