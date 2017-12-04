a37ad16a13c88343908f533d23f074860f9f4af8
kyuhyeok <kyu4745@naver.com>
2017-12-04 16:57:27 +0900

    총 페이지수 myutil

--- duospace/src/main/java/com/duospace/common/MyUtil.java
+++ duospace/src/main/java/com/duospace/common/MyUtil.java
@@ -1,5 +1,21 @@
 package com.duospace.common;
 
+import org.springframework.stereotype.Service;
+
+@Service("myUtil")
 public abstract class MyUtil {
-    
+	//********************************************
+	// 총페이지 수 구하기
+	public int pageCount(int numPerPage, int dataCount) {
+		int pageCount=0;
+		
+		if(dataCount > 0) {
+			if(dataCount % numPerPage == 0)
+				pageCount=dataCount/numPerPage;
+			else
+				pageCount=dataCount/numPerPage+1;
+		}
+		
+		return pageCount;
+	}
 }
