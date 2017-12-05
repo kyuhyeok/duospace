8329a00c9607e5e95e35c19706fc35edf73e7222
Kim jong ki <rlawhdrl3702@naver.com>
2017-12-05 11:00:18 +0900

    duogram 디자인 수정 완료

--- duospace/src/main/java/com/duospace/duogram/Duogram.java
+++ duospace/src/main/java/com/duospace/duogram/Duogram.java
@@ -3,86 +3,130 @@ package com.duospace.duogram;
 import org.springframework.web.multipart.MultipartFile;
 
 public class Duogram {
-    private int num, listNum;
-    private String userName, userId, subject, content, created;
-    private int hitCount;
 	private MultipartFile upload;  // <input type='file' name='upload' ..
+	private int replyNum, num, answer, memberNum, friendNum;
+	private String Intro, CoverSaveFileName, ProFileSaveFileName;
+	private String company, school, home, created, updated, content;
 	
-	private String saveFilename;
-	private String originalFilename;
 	
-	private int replyCount;
-	
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public int getListNum() {
-		return listNum;
-	}
-	public void setListNum(int listNum) {
-		this.listNum = listNum;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getSubject() {
-		return subject;
-	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getCreated() {
-		return created;
-	}
-	public void setCreated(String created) {
-		this.created = created;
-	}
-	public int getHitCount() {
-		return hitCount;
-	}
-	public void setHitCount(int hitCount) {
-		this.hitCount = hitCount;
-	}
 	public MultipartFile getUpload() {
 		return upload;
 	}
+
 	public void setUpload(MultipartFile upload) {
 		this.upload = upload;
 	}
-	public String getSaveFilename() {
-		return saveFilename;
+
+	public int getReplyNum() {
+		return replyNum;
 	}
-	public void setSaveFilename(String saveFilename) {
-		this.saveFilename = saveFilename;
+
+	public void setReplyNum(int replyNum) {
+		this.replyNum = replyNum;
 	}
-	public String getOriginalFilename() {
-		return originalFilename;
+
+	public int getNum() {
+		return num;
 	}
-	public void setOriginalFilename(String originalFilename) {
-		this.originalFilename = originalFilename;
+
+	public void setNum(int num) {
+		this.num = num;
 	}
-	public int getReplyCount() {
-		return replyCount;
+
+	public int getAnswer() {
+		return answer;
 	}
-	public void setReplyCount(int replyCount) {
-		this.replyCount = replyCount;
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
 	}
+
+	public int getMemberNum() {
+		return memberNum;
+	}
+
+	public void setMemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+
+	public int getFriendNum() {
+		return friendNum;
+	}
+
+	public void setFriendNum(int friendNum) {
+		this.friendNum = friendNum;
+	}
+
+	public String getIntro() {
+		return Intro;
+	}
+
+	public void setIntro(String intro) {
+		Intro = intro;
+	}
+
+	public String getCoverSaveFileName() {
+		return CoverSaveFileName;
+	}
+
+	public void setCoverSaveFileName(String coverSaveFileName) {
+		CoverSaveFileName = coverSaveFileName;
+	}
+
+	public String getProFileSaveFileName() {
+		return ProFileSaveFileName;
+	}
+
+	public void setProFileSaveFileName(String proFileSaveFileName) {
+		ProFileSaveFileName = proFileSaveFileName;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(String updated) {
+		this.updated = updated;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
 }
--- duospace/src/main/webapp/WEB-INF/view/duoGram/main.jsp
+++ duospace/src/main/webapp/WEB-INF/view/duoGram/main.jsp
@@ -72,7 +72,7 @@ line-height: 1.6;
 
 <!-- header -->
 <form>
-	<div style="background: white; position: fixed; top:0px; margin: 0px; width: 100%; height:80px; border-bottom: 1px solid #eee" align="center">
+	<div style="background: #172A40; position: fixed; top:0px; margin: 0px; width: 100%; height:80px; border-bottom: 15px solid #A7C7C5" align="center">
 		<table style="height: 80px; width: 935px; margin: 0px;">
 			<tr>
 				<td align="left" width="311px">
@@ -81,14 +81,14 @@ line-height: 1.6;
 				</td>
 				
 				<td align="center"> 
-					<a href="<%=cp%>/duogram" class="logo" style="font-size: 46px; text-decoration:none; color: black;">Duo Gram</a>			
+					<a href="<%=cp%>/duogram" class="logo" style="font-size: 46px; text-decoration:none; color: white;">Duo Gram</a>			
 				</td>
 			
 				<td align="right" width="311px">
-					<a href="#" style="font-family:'나눔고딕'; font-size: 12px; color: black; font-weight: bold; text-decoration:none;"> 친구 |</a>
-					<a href="<%=cp%>/main" style="font-family: '나눔고딕'; font-size: 12px; color: black; font-weight: bold; text-decoration:none;"> HOME   |</a>
-					<a href="#" style="font-family: '나눔고딕'; font-size: 12px; color: black; font-weight: bold; text-decoration:none;"> 기록 |</a>
-					<a href="#" style="font-family: '나눔고딕'; font-size: 12px; color: black; font-weight: bold; text-decoration:none;"> 마이페이지</a>
+					<a href="#" style="font-family:'나눔고딕'; font-size: 12px; color: white; font-weight: bold; text-decoration:none;"> 친구 |</a>
+					<a href="<%=cp%>/main" style="font-family: '나눔고딕'; font-size: 12px; color: white; font-weight: bold; text-decoration:none;"> HOME   |</a>
+					<a href="#" style="font-family: '나눔고딕'; font-size: 12px; color: white; font-weight: bold; text-decoration:none;"> 기록 |</a>
+					<a href="#" style="font-family: '나눔고딕'; font-size: 12px; color: white; font-weight: bold; text-decoration:none;"> 마이페이지</a>
 				</td>
 			</tr>
 		</table>
