9978fcee636cac021c89b468072f6869c9cab8e3
ysy <tndduf123@naver.com>
2017-12-07 21:02:56 +0900

    12월7일.....수고했어요~

--- duospace/src/main/java/com/duospace/community/commoim/CommoimService.java
+++ duospace/src/main/java/com/duospace/community/commoim/CommoimService.java
@@ -1,6 +1,9 @@
 package com.duospace.community.commoim;
 
+import java.util.List;
+import java.util.Map;
 
 public interface CommoimService {
 	public int insertCommoim(Commoim dto);
+	public List<Commoim> listCommoim(Map<String, Object> map);
 }
